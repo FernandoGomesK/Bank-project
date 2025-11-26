c77ac897488e1525336d2646f384313e4a447b93
FernandoGomesK <fernandogomest94@gmail.com>
2025-11-25 22:29:52 -0300

    updating main and error handlers

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from routes import client_routes, branch_routes
 from utils.exceptions.BranchExceptions import BranchAlreadyExistsException
 from utils.exceptions.error_handlers import branch_exists_handler
 from utils.exceptions.client_exceptions import ClientAlreadyExistsException, ClientDoesntHaveCNPJException, ClientDoesntHaveCPFException
-from utils.exceptions.error_handlers import client_exists_handler, client_missing_cnpj_handler, client_missing_cpf_handler
+from utils.exceptions.error_handlers import client_exists_handler, client_missing_cnpj_handler, client_missing_cpf_handler, branch_doesnt_exist_handler
 
 # 1. Cria as tabelas no banco de dados automaticamente
 Base.metadata.create_all(bind=engine)
@@ -19,6 +19,7 @@ app = FastAPI()
 app.add_exception_handler(BranchAlreadyExistsException, branch_exists_handler)
 app.add_exception_handler(ClientAlreadyExistsException, client_exists_handler)
 app.add_exception_handler(ClientDoesntHaveCNPJException, client_missing_cnpj_handler)
+app.add_exception_handler(ClientDoesntHaveCPFException, client_missing_cpf_handler)
 app.add_exception_handler(ClientDoesntHaveCPFException, client_missing_cpf_handler)
 
 
--- utils/exceptions/error_handlers.py
+++ utils/exceptions/error_handlers.py
@@ -1,7 +1,8 @@
 from fastapi import Request
 from fastapi.responses import JSONResponse
 from utils.exceptions.BranchExceptions import BranchAlreadyExistsException
-from utils.exceptions.client_exceptions import ClientAlreadyExistsException, ClientDoesntHaveCNPJException, ClientDoesntHaveCPFException
+from utils.exceptions.client_exceptions import (ClientAlreadyExistsException, ClientDoesntHaveCNPJException, 
+                                                ClientDoesntHaveCPFException, BranchDoesntExistException)
 
 async def branch_exists_handler(request: Request, exc: BranchAlreadyExistsException):
     return JSONResponse(
@@ -37,4 +38,13 @@ async def client_missing_cnpj_handler(request: Request, exc: ClientDoesntHaveCNP
             "erro": "Erro de Validação",
             "mensagem": exc.message,
             "id_tentado": exc.client_id},
+    )
+    
+async def branch_doesnt_exist_handler(request: Request, exc: BranchDoesntExistException):
+    return JSONResponse(
+        status_code=404,
+        content={
+            "erro": "Recurso Não Encontrado",
+            "mensagem": exc.message,
+            "id_tentado": exc.branch_id},
     )
